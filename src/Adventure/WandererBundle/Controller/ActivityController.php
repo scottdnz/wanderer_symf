acf12e4502665278f735dddcbfc152271a979d52
scottd <scottdnz@hotmail.com>
2014-04-23 17:03:52 +1200

    Missed files

---
+++ src/Adventure/WandererBundle/Controller/ActivityController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Adventure\WandererBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+//require_once __DIR__ . "/../Lib/Init/location_maker.php";
+
+class ActivityController extends Controller {
+
+  public function mainAction() {
+    return $this->render('AdventureWandererBundle:Activity:main.html.twig');
+  }
+  
+}
