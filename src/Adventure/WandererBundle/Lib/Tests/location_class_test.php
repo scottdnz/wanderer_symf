2a55bfd7a6de024b809176c1aa7316e9cadaddb0
scottd <scottdnz@hotmail.com>
2014-06-17 12:44:48 +1200

    location change test file - try_move works

--- src/Adventure/WandererBundle/Lib/Tests/location_class_test.php
+++ src/Adventure/WandererBundle/Lib/Tests/location_class_test.php
@@ -21,7 +21,7 @@ class Location {
   
   
   private function set_error($beginning, $error_msg) {
-    $this->error .= sprintf("Error - %s %s. ", $beginning, $error_msg);
+    $this->_error .= sprintf("Error - Type: %s, %s", $beginning, $error_msg);
   }
   
   
@@ -46,7 +46,8 @@ class Location {
       $this->x_val = intval($obj->x_val);
       $this->y_val = intval($obj->y_val);
       $this->description = $obj->description;
-      $this->image = $obj->image;
+      $this->image = $obj->image;      
+           
       $this->exits = array("n"=> intval($exits->n),
         "ne"=> intval($exits->ne),
         "e"=> intval($exits->e),
@@ -98,44 +99,95 @@ class Location {
   }
   
   
-  public function move($direction) {
+  public function try_move($direction) {
+    global $Y_LIMIT;
+    global $X_LIMIT;
     $conf = "";
+    $this->clear_error();
+//    var_dump($this->exits);
     switch ($direction) {
   	case "n":
-  	  if ( ($this->y_val < $Y_LIMIT) && ($this->exits["n"] == 1) ) { 
-          $this->y_val++;
-          $conf .= "You move north. ";
+  	  if ( ($this->y_val >0) && ($this->exits["n"] == 1) ) { 
+        $conf .= "You move north. ";
   	  }
   	  else {
-  	    $this->set_error("You cannot go north. ", "");
+  	    $this->set_error("Moving", "You cannot go north. ");
   	  }
       break;
     case "ne":
-      if ( ($this->y_val < $Y_LIMIT) && ($this->x_val < $X_LIMIT) && 
+      if ( ($this->y_val > 0 ) && ($this->x_val < $X_LIMIT) && 
         ($this->exits["ne"] == 1) ) {
-        $this->y_val++;
-        $this->x_val++;
         $conf .= "You move northeast. ";
       }
       else {
-        $this->set_error("You cannot go northeast. ", "");
+        $this->set_error("Moving", "You cannot go northeast. ");
       }
       break;
     case "e":
+      if ( ($this->x_val < $X_LIMIT) && ($this->exits["e"] == 1) ) { 
+        $conf .= "You move east. ";
+  	  }
+  	  else {
+  	    $this->set_error("Moving", "You cannot go east. ");
+  	  }
       break;
     case "se":
+       if ( ($this->y_val >= 0) && ($this->x_val < $X_LIMIT) && 
+        ($this->exits["se"] == 1) ) {
+        $conf .= "You move southeast. ";
+      }
+      else {
+        $this->set_error("Moving", "You cannot go southeast. ");
+      }  
       break;
     case "s":
+      if ( ($this->y_val < $Y_LIMIT) && ($this->exits["s"] == 1) ) {
+        $conf .= "You move south. ";
+      }
+      else {
+        $this->set_error("Moving", "You cannot go south. ");
+      }  
       break;
     case "sw":
+      if ( ($this->y_val < $Y_LIMIT) && ($this->x_val > 0) && ($this->exits["sw"] == 1) ) {
+        $conf .= "You move southwest. ";
+      }
+      else {
+        $this->set_error("Moving", "You cannot go southwest. ");
+      }  
       break;
     case "w":
+      if ( ($this->x_val > 0) && ($this->exits["w"] == 1) ) {
+       $conf .= "You move west. ";
+      }
+      else {
+        $this->set_error("Moving", "You cannot go west. ");
+      }  
       break;
     case "nw":
+      if ( ($this->x_val > 0) && ($this->y_val > 0) && ($this->exits["nw"] == 1) ) {
+       $conf .= "You move northwest. ";
+      }
+      else {
+        $this->set_error("Moving", "You cannot go northwest. ");
+      }  
       break;
     case "up":
+      if ($this->exits["up"] == 1) {
+        $conf .= "You climb up. ";
+      }
+      else {
+        $this->set_error("Moving", "You cannot climb up. ");
+      }  
       break;
     case "down":
+      if ($this->exits["down"] == 1) {
+        $conf .= "You climb down. ";
+      }
+      else {
+        $this->set_error("Moving", "You cannot climb down. ");
+      }  
+
       break;
     }
     return $conf;
--- src/Adventure/WandererBundle/Lib/Tests/test_location_change.php
+++ src/Adventure/WandererBundle/Lib/Tests/test_location_change.php
@@ -1,20 +1,58 @@
 <?php
 
-require("../../Entity/DBConnection.php");
+//require("../../Entity/DBConnection.php");
+require_once("../central_config.php");
+require_once("location_class_test.php");
 
-require("location_class_test.php");
-
-use Adventure\WandererBundle\Entity\DBConnection;
+//use Adventure\WandererBundle\Entity\DBConnection;
 
 
-class DBConnectionTest extends PHPUnit_Framework_TestCase {
+/**
+ * Reads an XML document with multiple locations and extracts the single location
+ * with matching x & y values.
+*/
+function get_start_location_xml($xml_strg) {
+    $obj = simplexml_load_string($xml_strg);
+    $start_y = 1;
+    $start_x = 3;
+    foreach ($obj->location as $loc) {
+      if ( ($loc->y_val == $start_y) && ($loc->x_val == $start_x) ) {
+        $start_loc = $loc;
+        break;
+      }
+    }
+    // Copy XML element into standalone XML doc
+    $obj = new SimpleXMLElement("<locations />");    
+    $loc_node = $obj->addChild("location");
+    foreach($start_loc->children() as $ch) {
+      // Exits is a node on its own with 10 children
+      if ($ch->getName() == "exits") {
+        $exits = $loc_node->addChild("exits");
+        foreach($ch->children() as $exit_ch) {
+          $exit_node = $exits->addChild($exit_ch->getName(), intval($exit_ch));
+        }
+      }
+      else {
+        $ch_node = $loc_node->addChild($ch->getName(), $ch);
+      }
+    }
+    return $obj->asXML();
+}
 
-  protected static $conn;
+
+class LocationTest extends PHPUnit_Framework_TestCase {
+
+  //protected static $conn;
+  protected static $xml_strg;
+  protected static $start_location_xml;
+  protected static $loc;
+  protected static $X_LIMIT;
+  protected static $Y_LIMIT;
   
 
   public static function setUpBeforeClass() {
     // Comment the next lines out to test the connect/close methods
-    $vals = yaml_parse_file("../../../../../app/config/parameters.yml");
+    /*$vals = yaml_parse_file("../../../../../app/config/parameters.yml");
     $params = $vals["parameters"];
     $db_params = array("hostname"=> $params["database_host"],
     "username"=> $params["database_user"],
@@ -24,38 +62,110 @@ class DBConnectionTest extends PHPUnit_Framework_TestCase {
     );
 	  self::$conn = new DBConnection($db_params);
     self::$conn->connect();
+    */
+    //self::$xml_strg = file_get_contents("location_req.xml");
+    self::$xml_strg = file_get_contents("../Init/locations_20140616.xml");
+    self::$start_location_xml = get_start_location_xml(self::$xml_strg);
+    self::$loc = new Location();
   }
   
   
   public static function tearDownAfterClass() {
     // Comment this out if needed
-    $ignored = self::$conn->close();
+    //$ignored = self::$conn->close();
     //remove_test_data();  
   }
+
   
   
   public function test_set_from_XML() {   
-    $xml_strg = file_get_contents("location_req.xml");    
-    $loc = new Location();
-    $loc->set_from_XML($xml_strg);
-    $this->assertEquals($loc->get_error(), "");
+    self::$loc->set_from_XML(self::$start_location_xml);
+    $this->assertEquals(self::$loc->get_error(), "");
   }
   
   
+  
   public function test_get_as_dict() {
-    $xml_strg = file_get_contents("location_req.xml");
-    $loc = new Location();
-    $loc->set_from_XML($xml_strg);
-    
-    $loc_dict = $loc->get_as_dict();
+    self::$loc->set_from_XML(self::$start_location_xml);
+    $loc_dict = self::$loc->get_as_dict();
+    $this->assertEquals(self::$loc->get_error(), "");
     /*echo "short_lbl: " . $loc_dict["short_lbl"] . "\n";
     $exit_vals = array("n", "ne", "e", "se", "s", "sw", "w", "nw", "up", "down");
     foreach ($exit_vals as $exit_val) {
       echo $exit_val . ": " . $loc_dict["exits"][$exit_val] . "\n";
     }*/
-    $this->assertEquals($loc->get_error(), "");
   }
   
+      
+  public function test_move_north() {   
+    self::$loc->set_from_XML(self::$start_location_xml);
+    $confirmation = self::$loc->try_move("n");
+    $this->assertEquals($confirmation, "You move north. ");
+  }
+  
+  
+  public function test_move_northeast_no_exit() {   
+    self::$loc->set_from_XML(self::$start_location_xml);
+    $confirmation = self::$loc->try_move("ne");
+    $this->assertEquals(self::$loc->get_error(), "Error - Type: Moving, You cannot go northeast. ");
+  }
+  
+  
+  public function test_move_east() {   
+    self::$loc->set_from_XML(self::$start_location_xml);
+    $confirmation = self::$loc->try_move("e");
+    $this->assertEquals($confirmation, "You move east. ");
+  }
+  
+  
+  public function test_move_southeast() {   
+    self::$loc->set_from_XML(self::$start_location_xml);
+    $confirmation = self::$loc->try_move("se");
+    $this->assertEquals($confirmation, "You move southeast. ");
+  }
+  
+  
+  public function test_move_south_no_exit() {   
+    self::$loc->set_from_XML(self::$start_location_xml);
+    $confirmation = self::$loc->try_move("s");
+    //echo self::$loc->get_error();
+    $this->assertEquals(self::$loc->get_error(), "Error - Type: Moving, You cannot go south. ");
+  }
+  
+  
+  public function test_move_southwest_no_exit() {   
+    self::$loc->set_from_XML(self::$start_location_xml);
+    $confirmation = self::$loc->try_move("sw");
+    $this->assertEquals(self::$loc->get_error(), "Error - Type: Moving, You cannot go southwest. ");
+  }
+  
+  
+  public function test_move_west() {   
+    self::$loc->set_from_XML(self::$start_location_xml);
+    $confirmation = self::$loc->try_move("w");
+    $this->assertEquals($confirmation, "You move west. ");
+  }
+  
+  
+  public function test_move_northwest_no_exit() {   
+    self::$loc->set_from_XML(self::$start_location_xml);
+    $confirmation = self::$loc->try_move("nw");
+    $this->assertEquals(self::$loc->get_error(), "Error - Type: Moving, You cannot go northwest. ");
+  }
+  
+  
+  public function test_move_up_no_exit() {   
+    self::$loc->set_from_XML(self::$start_location_xml);
+    $confirmation = self::$loc->try_move("up");
+    $this->assertEquals(self::$loc->get_error(), "Error - Type: Moving, You cannot climb up. ");
+  }
+  
+  
+  public function test_move_down_no_exit() {   
+    self::$loc->set_from_XML(self::$start_location_xml);
+    $confirmation = self::$loc->try_move("down");
+    $this->assertEquals(self::$loc->get_error(), "Error - Type: Moving, You cannot climb down. ");
+  }
     
   /*
   public function test_() {   
